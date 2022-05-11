7248becd4ac365ce87b64349a3afd72bc438dd6e
matiasconsiglio <macastro1@uc.cl>
2022-05-11 15:54:20 +0000

    bug found trying to restart game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,43 @@
+
 const instructions = document.querySelector('.instructions'); // We call constants in java from html the ones we want to make changes on them.
 const scoreboard = document.querySelector('.scoreboard');
 const gamePlay = document.querySelector('.gamePlay');
 const score = document.querySelector('.score');
 const button = document.getElementById('button');
-
+let circleClick = false;
 let play=false;//not playing
-
+let time = 0;
 button.addEventListener('click', tapAndPlay);//used to listen when the user press the button to start the game.
 
 function tapAndPlay(){//inmediate effect of pressing the play button, clean page for game, calls for gameStart function to start the game.
     play=true;
     score.innerHTML='0';
     instructions.innerHTML='';
-    //scoreboard.style.display='none';
     button.style.display='none';
     gameStart();
- 
+    
+    
 }
-
-
 
 function randomNumber(n){//function that generates an interger random number between 0 and n.
     let random = Math.floor(Math.random()*n)
     return random;
 }
-//let initialTime = 0;
+
 let gameTimespace = {};
 let points = 0;
+
 function gameStart(){//function that calls in action de circle that the user need to press in the game, it will appear in time by a random number between 0 and 3000 miliseconds
+    
     gameTimespace = setTimeout(circle, randomNumber(3000 - points*20));
-    //initialTime = new Date().getTime();
+    
+    
 }
 
+let chance={};
 
-
-let visibility = false;
 function circle(){
+    circleClick=false;
     let e = document.createElement('div');
     e.classList.add('circle');
     e.style.height = (125 -1.5*points)+'px';
@@ -45,57 +47,60 @@ function circle(){
     e.style.top = (randomNumber((gamePlay.clientHeight)/1.5 +1 - 150) + 150) + 'px';
     e.start = new Date().getTime();
     
-    e.addEventListener('click',tap);
-    
+   
+    e.addEventListener('click',tap)
     gamePlay.appendChild(e);
-    visibility = true;
-    setTimeout(function(){ //using setTimeout function
-    e.style.display ='inline'; //displaying the button again after 3000ms or 3 seconds
+    
+    chance = setTimeout(function (){ 
+    e.style.display ='inline';
+    if (circleClick === false){
+        reset();
+        
+
+    }
+       
     }
     ,2000 - points*20); 
-    visibility = false;
-   // if(initialTime>(2000 - points*20)){
-      //  reset();
+    //if (circleClick = true){
+       // circleClick = false;
+        //return circleClick;
     //}
-    newgame();
+    
+    
 }
+
+
 
 function count(points){
     return points;
 }
 
-function newgame(){
-    let start = element.target.start;
-    let finish = new Date().getTime();
-    let duration = (finish - start);
-    let allowDuration = (2 - points*25);
-    if(duration > allowDuration){
-        gamePlay.children[0].remove();
-        reset();
-    } else(null)
-}
 
 function tap(element){
-    let start = element.target.start;
-    let finish = new Date().getTime();
-    let duration = (finish - start);
-    let allowDuration = (2000 - points*25);
+    circleClick = true;
     clearTimeout(gameTimespace);
-    if( duration <=allowDuration ){
-        gamePlay.children[0].remove();
-        points++
-        gameTimespace = setTimeout(circle, randomNumber(3000 - points*20));
-        score.innerHTML=` ${count(points)} `
-    } 
+    gamePlay.children[0].remove();
+    points++
+    gameTimespace = setTimeout(circle, randomNumber(3000 - points*20));
+    score.innerHTML=` ${count(points)} `
+     
+
+    
+
 }
 
+
 function reset(){
+    
+    
     alert('heelo');
+    gamePlay.children[0].remove();
     clearTimeout(gameTimespace);
+    clearTimeout(chance);
     play=false;
     score.innerHTML=` ${count(points)} `;
     instructions.innerHTML='';
     button.style.display='block';
     //points = 0;
-
+  
 }
