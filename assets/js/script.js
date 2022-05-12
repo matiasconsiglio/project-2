394e5bb771a93d2ccbd6a512ef30921101c2f889
matiasconsiglio <macastro1@uc.cl>
2022-05-12 11:32:11 +0000

    Add CSS to html, circle, gamePlay, logo , heading, instructions, container for button, button, score. Fix Bug so now game works properly. Add function tapandplay, randomnumber, gamestart, circle, count, tap, reset. to Java.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,16 @@ const scoreboard = document.querySelector('.scoreboard');
 const gamePlay = document.querySelector('.gamePlay');
 const score = document.querySelector('.score');
 const button = document.getElementById('button');
-let circleClick = false;
-let play=false;//not playing
+let circleClick;
 let time = 0;
 button.addEventListener('click', tapAndPlay);//used to listen when the user press the button to start the game.
 
 function tapAndPlay(){//inmediate effect of pressing the play button, clean page for game, calls for gameStart function to start the game.
-    play=true;
     score.innerHTML='0';
     instructions.innerHTML='';
     button.style.display='none';
     gameStart();
+    points=0;
     
     
 }
@@ -30,39 +29,35 @@ let points = 0;
 function gameStart(){//function that calls in action de circle that the user need to press in the game, it will appear in time by a random number between 0 and 3000 miliseconds
     
     gameTimespace = setTimeout(circle, randomNumber(3000 - points*20));
-    
+    circleClick = false;
     
 }
 
 let chance={};
 
 function circle(){
-    circleClick=false;
+    
     let e = document.createElement('div');
     e.classList.add('circle');
-    e.style.height = (125 -1.5*points)+'px';
-    e.style.width = (125 -1.5*points)+'px';
-    e.style.backgroundColor = 'rgb(' + points*3 + ',' + points*3 + ',' + points*3 + ')';
+    e.style.height = (125 -1.24*points)+'px';
+    e.style.width = (125 -1.24*points)+'px';
+    e.style.backgroundColor = 'rgb(' + (2 +points*2) + ',' + (44 + points*2) + ',' + (85 + points) + ')';
     e.style.left = (randomNumber((gamePlay.clientWidth)/1.5 +1 - 150) + 150) + 'px';
     e.style.top = (randomNumber((gamePlay.clientHeight)/1.5 +1 - 150) + 150) + 'px';
     e.start = new Date().getTime();
-    
+    gamePlay.appendChild(e);
    
     e.addEventListener('click',tap)
-    gamePlay.appendChild(e);
     
+    circleClick = false;
     chance = setTimeout(function (){ 
     e.style.display ='inline';
     if (circleClick === false){
-        reset();
-        
-
+        reset();}   
     }
-       
-    }
-    ,2000 - points*20); 
+    ,2500 - points*20); 
     //if (circleClick = true){
-       // circleClick = false;
+       // ;
         //return circleClick;
     //}
     
@@ -70,37 +65,29 @@ function circle(){
 }
 
 
-
 function count(points){
     return points;
 }
 
-
-function tap(element){
+function tap(){
     circleClick = true;
     clearTimeout(gameTimespace);
+    clearTimeout(chance);
     gamePlay.children[0].remove();
     points++
     gameTimespace = setTimeout(circle, randomNumber(3000 - points*20));
     score.innerHTML=` ${count(points)} `
-     
-
-    
 
 }
 
-
 function reset(){
-    
-    
-    alert('heelo');
+
     gamePlay.children[0].remove();
     clearTimeout(gameTimespace);
     clearTimeout(chance);
-    play=false;
-    score.innerHTML=` ${count(points)} `;
-    instructions.innerHTML='';
+    score.innerHTML='';
+    instructions.innerHTML=`You Lost... Your Score: ${count(points)} `;
     button.style.display='block';
-    //points = 0;
+    //points = 0;` ${count(points)} `
   
 }
