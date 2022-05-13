0794910ec8527bfabac733bce9787085bff5cdc2
matiasconsiglio <macastro1@uc.cl>
2022-05-13 08:50:52 +0000

    Add Hard difficulty, Add event listener for new button, add function tap and play hard, add function game start two, add function circle two add function tap two

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ let points=0;
 let chance={};
 let gameTimespace={};
 button.addEventListener('click', tapAndPlay);//used to listen when the user press the button to start the game.
+buttonTwo.addEventListener('click', tapAndPlayHard);
 
 function tapAndPlay(){//inmediate effect of pressing the play button, clean page for game, calls for gameStart function to start the game.
     score.innerHTML='0';
@@ -20,6 +21,14 @@ function tapAndPlay(){//inmediate effect of pressing the play button, clean page
     button.style.display='none';
     buttonTwo.style.display='none';
     gameStart();
+    points=0; 
+}
+function tapAndPlayHard(){//inmediate effect of pressing the play button, clean page for game, calls for gameStart function to start the game.
+    score.innerHTML='0';
+    instructions.innerHTML='';
+    button.style.display='none';
+    buttonTwo.style.display='none';
+    gameStartTwo();
     points=0; 
 }
 
@@ -30,6 +39,11 @@ function randomNumber(n){//function that generates an interger random number bet
 
 function gameStart(){//function that calls in action de circle that the user need to press in the game, it will appear in time by a random number between 0 and 3000 miliseconds
     gameTimespace = setTimeout(circle, randomNumber(2000 - points*15));
+    circleClick = false; 
+}
+
+function gameStartTwo(){//function that calls in action de circle that the user need to press in the game, it will appear in time by a random number between 0 and 3000 miliseconds
+    gameTimespace = setTimeout(circleTwo, randomNumber(1500 - points*16));
     circleClick = false; 
 }
 
@@ -52,6 +66,25 @@ function circle(){
         },1000 - points*8); 
 }
 
+function circleTwo(){
+    let e = document.createElement('div');
+    e.classList.add('circle');
+    e.style.height = (100 -1.3*points)+'px';
+    e.style.width = (100 -1.3*points)+'px';
+    e.style.backgroundColor = 'rgb(' + (20 + points*3) + ',' + (20 + points*3) + ',' + (255) + ')';
+    e.style.left = (randomNumber((gamePlay.clientWidth) +1 - 110)) + 'px';
+    e.style.top = (randomNumber((gamePlay.clientHeight) +1 - 250)) + 'px';
+    e.start = new Date().getTime();
+    gamePlay.appendChild(e);
+    e.addEventListener('click',tapTwo)
+    circleClick = false;
+    chance = setTimeout(function (){ 
+    e.style.display ='inline'; 
+        if (circleClick === false){
+            reset();}   
+        },800 - points*9); 
+}
+
 function count(points){
     return points;
 }
@@ -62,7 +95,17 @@ function tap(){
     clearTimeout(chance);
     gamePlay.children[0].remove();
     points++;
-    gameTimespace = setTimeout(circle, randomNumber(2000 - points*15));
+    gameTimespace = setTimeout(circle, randomNumber(2000 - points*16));
+    score.innerHTML=` ${count(points)} `;
+}
+
+function tapTwo(){
+    circleClick = true;
+    clearTimeout(gameTimespace);
+    clearTimeout(chance);
+    gamePlay.children[0].remove();
+    points++;
+    gameTimespace = setTimeout(circleTwo, randomNumber(1500 - points*12));
     score.innerHTML=` ${count(points)} `;
 }
 
@@ -75,4 +118,3 @@ function reset(){
     button.style.display='block';
     buttonTwo.style.display='block';
 }
-
