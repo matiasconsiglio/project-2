4e809763308c9a204bda8885ae35e86daae2ea9d
matiasconsiglio <macastro1@uc.cl>
2022-05-13 07:52:49 +0000

    Add Second button for hard difficulty

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,14 @@ const scoreboard = document.querySelector('.scoreboard');
 const gamePlay = document.querySelector('.gamePlay');
 const score = document.querySelector('.score');
 const button = document.getElementById('button');
+
+
+
 let circleClick;
-let time = 0;
+let time=0;
+let points=0;
+let chance={};
+let gameTimespace={};
 button.addEventListener('click', tapAndPlay);//used to listen when the user press the button to start the game.
 
 function tapAndPlay(){//inmediate effect of pressing the play button, clean page for game, calls for gameStart function to start the game.
@@ -13,30 +19,20 @@ function tapAndPlay(){//inmediate effect of pressing the play button, clean page
     instructions.innerHTML='';
     button.style.display='none';
     gameStart();
-    points=0;
-    
-    
+    points=0; 
 }
 
 function randomNumber(n){//function that generates an interger random number between 0 and n.
-    let random = Math.floor(Math.random()*n)
+    let random = Math.floor(Math.random()*n);
     return random;
 }
 
-let gameTimespace = {};
-let points = 0;
-
 function gameStart(){//function that calls in action de circle that the user need to press in the game, it will appear in time by a random number between 0 and 3000 miliseconds
-    
     gameTimespace = setTimeout(circle, randomNumber(2000 - points*15));
-    circleClick = false;
-    
+    circleClick = false; 
 }
 
-let chance={};
-
 function circle(){
-    
     let e = document.createElement('div');
     e.classList.add('circle');
     e.style.height = (125 -1.24*points)+'px';
@@ -46,24 +42,14 @@ function circle(){
     e.style.top = (randomNumber((gamePlay.clientHeight) +1 - 250)) + 'px';
     e.start = new Date().getTime();
     gamePlay.appendChild(e);
-
     e.addEventListener('click',tap)
-    
     circleClick = false;
     chance = setTimeout(function (){ 
-    e.style.display ='inline';
-    if (circleClick === false){
-        reset();}   
-    }
-    ,1000 - points*8); 
-    //if (circleClick = true){
-       // ;
-        //return circleClick;
-    //}
-    
-    
+    e.style.display ='inline'; 
+        if (circleClick === false){
+            reset();}   
+        },1000 - points*8); 
 }
-
 
 function count(points){
     return points;
@@ -74,20 +60,17 @@ function tap(){
     clearTimeout(gameTimespace);
     clearTimeout(chance);
     gamePlay.children[0].remove();
-    points++
+    points++;
     gameTimespace = setTimeout(circle, randomNumber(2000 - points*15));
-    score.innerHTML=` ${count(points)} `
-
+    score.innerHTML=` ${count(points)} `;
 }
 
 function reset(){
-
     gamePlay.children[0].remove();
     clearTimeout(gameTimespace);
     clearTimeout(chance);
     score.innerHTML='';
     instructions.innerHTML=`You Lost... Your Score: ${count(points)} `;
     button.style.display='block';
-    //points = 0;` ${count(points)} `
-  
 }
+
